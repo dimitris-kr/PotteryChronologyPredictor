62ec2b98430e3dbfa3c8bf581d3ec80246017bef
dimitris <dkrikonis6@gmail.com>
2026-01-19 23:00:51 +0200

    small updates

--- src/app/public/login/login.ts
+++ src/app/public/login/login.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.scss',
+    selector: 'app-login',
+    imports: [
+        MatFormField,
+        MatLabel,
+        MatIcon,
+        MatInput,
+        MatIconButton
+    ],
+    templateUrl: './login.html',
+    styleUrl: './login.scss',
 })
 export class Login {
+    hidePassword: boolean = true;
 
+    togglePasswordVisibility(event: MouseEvent) {
+        this.hidePassword = !this.hidePassword;
+        event.stopPropagation();
+    }
 }
